4924ddee34498c4eeb118c2b7ea22ff78ff6cae8
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2023-10-09 22:14:28 -0700

    Fix cors configuration.

--- app/app/settings.py
+++ app/app/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,14 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:5173",
-    "https://localhost:5174",
-    "https://localhost:5175",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
 ]
 
 ROOT_URLCONF = 'app.urls'
